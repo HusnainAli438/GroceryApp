eb7a31d51bb064264e1e9815b3a588377696dfd5
HusnainAli438 <husnainali438@gmail.com>
2025-09-25 21:13:01 +0500

    Updated routes

--- backend/src/routes/basketRoutes.js
+++ backend/src/routes/basketRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { addToBasket, getBasketItems } from "../controllers/basketController.js";
+import { addToBasket, getBasketItems, updateBasketItem, deleteBasketItem } from "../controllers/basketController.js";
 const router = express.Router();
 
 router.get("/", getBasketItems);
 router.post("/", addToBasket);
+router.patch("/:itemName", updateBasketItem);
+router.delete("/:itemName", deleteBasketItem);
+
 export default router;
